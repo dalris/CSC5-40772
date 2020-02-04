63f99752ab1da32be9f1592f62c20898702189e8
dalris <dalrissyn@gmail.com>
2020-02-03 16:24:53 -0800

    Begin Project 1

---
+++ Proj/Project 1/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Danziel Nguyen
+ * Created on 2020/02/03
+ * Purpose:  Project 1: Sì-Wŭ-Liù (a.k.a. Cee-lo)
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number seed
+    
+    //Declare Variable Data Types and Constants
+    
+    //Initialize Variables
+    
+    //Process or map Inputs to Outputs
+    
+    //Display Outputs
+
+    //Exit stage right!
+    return 0;
+}
