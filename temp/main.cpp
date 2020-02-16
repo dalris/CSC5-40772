94cea63eb622c5cf2cbbf3aa43e497be17980a32
dalris <dalrissyn@gmail.com>
2020-02-16 03:05:34 -0800

    fixes

--- temp/main.cpp
+++ temp/main.cpp
@@ -10,38 +10,40 @@ using namespace std;
 
 //Global Constants Only, No Global Variables
 //Allowed like PI, e, Gravity, conversions, array dimensions necessary
-const int COLMAX=80;//Only 20 required, and 1 for null terminator
+const int COLMAX = 80;//Only 20 required, and 1 for null terminator
 
 //Function Prototypes Here
-int  read(char [][COLMAX],int &);//Outputs row and columns detected from input
-void sort(char [][COLMAX],int,int,const char[],const char[]);//Sort by row using strcmp();
-void print(const char [][COLMAX],int,int);//Print the sorted 2-D array
+int read(char [][COLMAX], int &);//Outputs row and columns detected from input
+void sort(char [][COLMAX], int, int, const char[], const char[]);//Sort by row using strcmp();
+void print(const char [][COLMAX], int, int);//Print the sorted 2-D array
 //int strcmp(char a[],char b[],char replace[],char with[]){
-int strcmp(char [],char [],const char [],const char []);//Replace sort order
+int strcmp(char [], char [], const char [], const char []);//Replace sort order
 
 //Program Execution Begins Here
-int main(int argc, char** argv) 
+int main(int argc, char** argv){
     //Declare all Variables Here
-    const int ROW=30;             //Only 20 required
+    const int ROW = 30;             //Only 20 required
     char array[ROW][COLMAX];      //Bigger than necessary 
-    int colIn,colDet,rowIn,rowDet;//Row, Col input and detected
-    char replace[COLMAX],with[COLMAX];//New sort order
+    int colIn, colDet, rowIn, rowDet;//Row, Col input and detected
+    char replace[COLMAX], with[COLMAX];//New sort order
     
     //Input the new sort order then sort
-    cout<<"Modify the sort order by swapping these characters."<<endl;
-    cin>>replace;
-    cout<<"With the following characters"<<endl;
-    cin>>with;
-    cout<<"Read in a 2 dimensional array of characters and sort by Row"<<endl;
-    cout<<"Input the number of rows <= 20"<<endl;
-    cin>>rowIn;
-    cout<<"Input the maximum number of columns <=20"<<endl;
-    cin>>colIn;
+    cout << "Modify the sort order by swapping these characters." =
+        << "\n";
+    cin >> replace;
+    cout << "With the following characters" << "\n";
+    cin >> with;
+    cout << "Read in a 2 dimensional array of characters and sort by Row"
+        << "\n";
+    cout << "Input the number of rows <= 20" << "\n";
+    cin >> rowIn;
+    cout << "Input the maximum number of columns <=20" << "\n";
+    cin >> colIn;
     
     //Now read in the array of characters and determine it's size
-    rowDet=rowIn;
-    cout<<"Now input the array."<<endl;
-    colDet=read(array,rowDet);
+    rowDet = rowIn;
+    cout << "Now input the array." << "\n";
+    colDet = read(array, rowDet);
     
     //Compare the size input vs. size detected and sort if same
     //Else output different size
