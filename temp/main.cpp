c013fa03283713230e1275ac5d0968372339e084
dalris <dalrissyn@gmail.com>
2020-02-16 02:54:59 -0800

    q8

--- temp/main.cpp
+++ temp/main.cpp
@@ -1,8 +1,6 @@
 /* 
- * File:   main.cpp
- * Author: Danziel Nguyen
- * Created on Feb 14
- * Purpose:  Sorting a 2-D array of characters if row and columns match
+ * Note:  This time you create the strcmp() function that takes into
+ * account the new sort order.
  */
 
 //System Libraries Here
@@ -10,51 +8,48 @@
 #include <cstring> //strlen(),strcmp(),strcpy()
 using namespace std;
 
-//User Libraries Here
-
 //Global Constants Only, No Global Variables
 //Allowed like PI, e, Gravity, conversions, array dimensions necessary
-const int COLMAX = 80;//Only 20 required, and 1 for null terminator
+const int COLMAX=80;//Only 20 required, and 1 for null terminator
 
 //Function Prototypes Here
-int read(char [][COLMAX], int &);//Outputs row and columns detected from input
-void sort(char [][COLMAX], int, int);//Sort by row
-void print(const char [][COLMAX], int, int);//Print the sorted 2-D array
+int  read(char [][COLMAX],int &);//Outputs row and columns detected from input
+void sort(char [][COLMAX],int,int,const char[],const char[]);//Sort by row using strcmp();
+void print(const char [][COLMAX],int,int);//Print the sorted 2-D array
+//int strcmp(char a[],char b[],char replace[],char with[]){
+int strcmp(char [],char [],const char [],const char []);//Replace sort order
 
 //Program Execution Begins Here
-int main(int argc, char** argv){
+int main(int argc, char** argv) 
     //Declare all Variables Here
-    const int ROW = 30;             //Only 20 required
+    const int ROW=30;             //Only 20 required
     char array[ROW][COLMAX];      //Bigger than necessary 
-    int colIn, colDet, rowIn, rowDet;//Row, Col input and detected
+    int colIn,colDet,rowIn,rowDet;//Row, Col input and detected
+    char replace[COLMAX],with[COLMAX];//New sort order
     
-    //Input the size of the array you are sorting
-    cout << "Read in a 2 dimensional array of characters and sort by Row" << "\n";
-    cout << "Input the number of rows <= 20" << "\n";
-    cin >> rowIn;
-    cout << "Input the maximum number of columns <= 20" << "\n";
-    cin >> colIn;
+    //Input the new sort order then sort
+    cout<<"Modify the sort order by swapping these characters."<<endl;
+    cin>>replace;
+    cout<<"With the following characters"<<endl;
+    cin>>with;
+    cout<<"Read in a 2 dimensional array of characters and sort by Row"<<endl;
+    cout<<"Input the number of rows <= 20"<<endl;
+    cin>>rowIn;
+    cout<<"Input the maximum number of columns <=20"<<endl;
+    cin>>colIn;
     
     //Now read in the array of characters and determine it's size
-    rowDet = rowIn;
-    cout << "Now input the array." << "\n";
-    colDet = read(array, rowDet);
+    rowDet=rowIn;
+    cout<<"Now input the array."<<endl;
+    colDet=read(array,rowDet);
     
     //Compare the size input vs. size detected and sort if same
     //Else output different size
-    if(rowDet == rowIn && colDet == colIn){
-        sort(array, rowIn, colIn);
-        cout << "The Sorted Array" << "\n";
-        print(array, rowIn, colIn);
-    }else{
-        if(rowDet != rowIn)
-        cout << (rowDet<rowIn?"Row Input size less than specified.":
-            "Row Input size greater than specified.") << "\n";
-        if(colDet != colIn)
-        cout << (colDet<colIn?"Column Input size less than specified.":
-            "Column Input size greater than specified.") << "\n";
-    }
-    
+    //if(rowDet==rowIn&&colDet==colIn){
+        sort(array,rowIn,colIn,replace,with);
+        cout<<"The Sorted Array"<<endl;
+        print(array,rowIn,colIn);
+        
     //Exit
     return 0;
 }
@@ -74,34 +69,33 @@ int read(char arr[][COLMAX], int &x){
     return longest;
 }
 
-void sort(char arr[][COLMAX], int row, int col){
-    char temp[40];
-    char shortest = arr[0][0];
+ void sort(char arr[][COLMAX], int row, int col, const char[],const char[]){
+    char* temp;
 
-    for(int i = 0; i < row; i++){
-        if(shortest > arr[i][0]){
-            shortest = arr[i][0];
-        }
-    }
-
-    do{
-        for(int i = 0; i < row; i++){
-            if(i != row - 1){
-                if(strcmp(arr[i], arr[i + 1]) > 0){
-                    strcpy(temp, arr[i]);
-                    strcpy(arr[i], arr[i + 1]);
-                    strcpy(arr[i + 1], temp);
-                }
+    for (int j = 0; j < row - 1; j++)
+    {
+        for (int i = j + 1; i < row; i++)
+        {
+            if (strcmp(arr[j], arr[i]) > 0)
+            {
+                temp = new char[strlen(arr[j]) + 1];
+                strcpy(temp, arr[j]);
+                strcpy(arr[j], arr[i]);
+                strcpy(arr[i], temp);
             }
         }
-    }while(arr[0][0] != shortest);
+    }
 }
 
 void print(const char arr[][COLMAX], int x, int y){
     for(int i = 0; i < x; i++){
-        for(int j = 0; j < y; j++){
+        for(int j = 0; j < strlen(arr[i]); j++){
             cout << arr[i][j];
         }
         cout << "\n";
     }
+}
+
+int strcmp(char [], char [], const char [], const char []){
+    
 }
